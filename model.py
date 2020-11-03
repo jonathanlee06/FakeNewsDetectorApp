3d75613adb423e24273047f880e9bdeef5daff25
jonathanlee06 <43515846+jonathanlee06@users.noreply.github.com>
2020-11-03 09:51:48 +0800

    Added Notebook

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import scikitplot.plotters as skplt
 import warnings
@@ -45,20 +46,39 @@ pickle.dump(classifier, open(filename, 'wb')) # Saving model
 
 # Get Accuracy Score
 score = accuracy_score(label_test, predict)
-print("Accuracy= %.2f%%" % (score*100))
+print("Accuracy Score: %.2f%%" % (score*100))
+
+X = vectorizer.transform(news['text'].astype('U'))
+kscore = cross_val_score(classifier, X, news['label'].values, cv=5)
+print(f'K Fold Accuracy: {round(kscore.mean()*100,2)}%')
+
+print("\nClassification Report")
+print(classification_report(label_test, predict))
+
+print("\nConfusion Matrix")
+print(confusion_matrix(label_test, predict))
 
 # --------- Testing using saved model ----------- #
 
-print("**********Using saved model**********")
-loaded_tfid = pickle.load(open(filename_vectorizer, 'rb')) # Load saved vectorizer
-loaded_classifier = pickle.load(open(filename, 'rb')) # Load saved classifier model
+# print("**********Using saved model**********")
+# loaded_tfid = pickle.load(open(filename_vectorizer, 'rb')) # Load saved vectorizer
+# loaded_classifier = pickle.load(open(filename, 'rb')) # Load saved classifier model
 
-text_to_predict = pd.Series(np.array(['Unique Creature Captured On Tape In Alaska']))
-transformed_text_to_predict = loaded_tfid.transform(text_to_predict)
+# test_fake_news = pd.read_csv('datasets/Fake.csv')
+# test_true_news = pd.read_csv('datasets/True.csv')
+# test_fake_news['label'] = 'Fake'
+# test_true_news['label'] = 'Real'
+# test_fake_text = test_fake_news['text'].astype('U')
 
-predict_from_model = loaded_classifier.predict(transformed_text_to_predict)
-print(predict_from_model)
+# transformed_text_to_predict = loaded_tfid.transform(test_text)
+
+# predict_from_model = loaded_classifier.predict(transformed_text_to_predict)
+# # print(predict_from_model)
+
+# # Get Accuracy Score
+# test_score = accuracy_score(label_test, predict_from_model)
+# print("Accuracy= %.2f%%" % (score*100))
 
 # --------- Testing using saved model ----------- #
 
-plot_cmat(label_test, predict) # Show Confusion Matrix
+#plot_cmat(label_test, predict) # Show Confusion Matrix
