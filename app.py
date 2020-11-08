967e3656bdd9c3ea45b8f68916e6334e5b910915
jonathanlee06 <43515846+jonathanlee06@users.noreply.github.com>
2020-11-08 16:31:31 +0800

    User Authentication Revamp

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 #Importing the Libraries
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template, redirect, url_for, Blueprint
+from flask import Flask, request, render_template, redirect, url_for, session, logging, flash
 from flask_cors import CORS
 from flask_login import login_required, current_user, login_user, logout_user
 from flask.logging import create_logger
+from flask_mysqldb import MySQL
+from functools import wraps
+import MySQLdb.cursors
+import re
 import os
 import joblib
 import pickle
 import flask
 import secrets
 from user_authentication import UserModel, loginManager, db
+# from db_config import mysql
+from werkzeug.security import generate_password_hash, check_password_hash
 import newspaper
 from newspaper import Article
 import urllib
@@ -26,17 +32,25 @@ CORS(app)
 app=flask.Flask(__name__,template_folder='templates')
 app.config['SECRET_KEY'] = secret
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'fakenewsapp_db'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/fakenewsapp_db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 log = create_logger(app)
 
-db.init_app(app)
+mysql = MySQL(app)
+#mysql.init_app(app)
+# db.init_app(app)
 loginManager.init_app(app)
 loginManager.login_view = 'login'
 
-@app.before_first_request
-def create_table():
-    db.create_all()
+# @app.before_first_request
+# def create_table():
+#     db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -55,58 +69,117 @@ def login():
 
 @app.route('/login', methods = ['POST'])
 def login_post():
+    msg = ''
+    email = ''
+
     if current_user.is_authenticated:
         return redirect('/history')
      
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        user = UserModel.query.filter_by(email = email).first()
+        # user = UserModel.query.filter_by(email = email).first()
 
-        if not user and not user.check_password(user.password, password):
-            return redirect(url_for('login'))
-            # return redirect(url_for('history')) 
-       
-        login_user(user)
-        print("Email is here " + email) 
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email LIKE %s', (email,)) 
+        account = cursor.fetchone()
+
+        if account:
+            password_db = account['password_hash']
+            if check_password_hash(password_db,password):
+                session['logged_in'] = True
+                session['username'] = account['username']
+                flash('You have successfully logged in!', 'success')
+                return redirect(url_for('history'))
+            else:
+                msg = "Invalid Password"
+                return render_template('login.html', msg=msg, email=email)
+
+            cursor.close()
+        else:
+            flash('Account with this email address does not exist! Please try again!', 'danger')
+
+        # if user and user.check_password(user.password, password):
+        #     login_user(user)
+        #     print("Email is here " + email) 
+        # else:
+        #     return redirect(url_for('login'))
+        #     # return redirect(url_for('history')) 
      
-    return redirect(url_for('history'))
+    return redirect(url_for('login'))
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if current_user.is_authenticated:
-        return redirect('/history')
+    msg = ''
+    email = ''
+    username = ''
+    # if current_user.is_authenticated:
+    #     return redirect('/history')
      
     if request.method == 'POST':
-
-        # email = request.form['email']
-        # username = request.form['username']
-        # password = request.form['password']
 
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
- 
-        if UserModel.query.filter_by(email=email).first():
-            # return ('Email already Present')
-            return redirect(url_for('register'))
-        else:
-            user = UserModel(email=email, username=username)
-            user.set_password(password)
-            db.session.add(user)
-            db.session.commit()
+        password_hash = generate_password_hash(password)
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email LIKE %s', (email,)) 
+        account = cursor.fetchone()
+
+        if account:
+            msg = "Email already exists!"
+            return render_template('register.html', msg=msg, email=email, username=username)
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email): 
+            msg = 'Invalid email address !'
+            return render_template('register.html', msg=msg)
+        elif not re.match(r'[A-Za-z0-9]+', username): 
+            msg = 'Username must contain only characters and numbers !'
+            return render_template('register.html', msg=msg)
+        elif not username or not password or not email: 
+            msg = 'Please fill out the form !'
+            return render_template('register.html', msg=msg)
+        else:     
+            cursor.execute("INSERT INTO users(email, username, password_hash) VALUES(%s,%s,%s)", (email, username, password_hash))
+            mysql.connection.commit()
+            cursor.close()
+            flash('You have successfully registered and you are allowed to login', 'success')
             return redirect(url_for('login'))
+ 
+        # if UserModel.query.filter_by(email=email).first():
+        #     # return ('Email already Present')
+        #     return redirect(url_for('register'))
+        # else:
+        #     user = UserModel(email=email, username=username)
+        #     user.set_password(password)
+        #     db.session.add(user)
+        #     db.session.commit()
+        #     return redirect(url_for('login'))
              
         
-    return render_template('register.html')
+    return render_template('register.html', msg=msg)
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Please login to gain access of this page', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/logout')
 def logout():
+    session.clear()
     logout_user()
     return redirect('/')
 
 @app.route('/history', methods = ['GET', 'POST'])
-@login_required
+@is_logged_in
+# @login_required
 def history():
     return render_template('history.html')
 
@@ -134,13 +207,13 @@ def predict():
     if (pred_outcome == "0"):
         outcome = "True"
     else:
-        if (pred_outcome == "Valid"):
+        if (pred_outcome == "REAL"):
             outcome = "True"
         else:
             outcome = "False"
     
     # return render_template('predict.html', prediction_text='{}'.format(pred[0]), url_input=url)
-    return render_template('predict.html', prediction_text=outcome, url_input=url, news=news)
+    return render_template('predict.html', prediction_text=pred_outcome, url_input=url, news=news)
     # return article.summary
 
 if __name__=="__main__":
--- user_authentication.py
+++ user_authentication.py
@@ -12,14 +12,14 @@ class UserModel(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True)
     username = db.Column(db.String(100))
-    password_hash = db.Column(db.String())
+    password_hash = db.Column(db.String(250))
  
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
      
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
- 
+
  
 @loginManager.user_loader
 def load_user(id):
